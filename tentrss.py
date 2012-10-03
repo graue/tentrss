656b854bc0632028789ce3e077aafdd47dcf1dcf
Scott Feeney <scott@oceanbase.org>
2012-10-02 21:35:22 -0500

    Print posts as raw JSON

--- tentrss.py
+++ tentrss.py
@@ -4,6 +4,9 @@ import requests
 app = Flask(__name__)
 
 
+tent_mime = 'application/vnd.tent.v0+json'
+
+
 @app.route('/')
 def front_page():
     return 'TentRSS!'
@@ -29,20 +32,41 @@ def user_feed():
         if rel != 'https://tent.io/rels/profile':
             continue
 
-        headers = {'accept': 'application/vnd.tent.v0+json'}
+        headers = {'accept': tent_mime}
         try:
             r = requests.get(href, timeout=5, headers=headers)
+            r.raise_for_status()
         except requests.exceptions.RequestException as e:
             app.logger.debug('exception loading %s: %s' % (href, repr(e)))
             continue
 
         # profile link worked, use it
         apiroots = r.json['https://tent.io/types/info/core/v0.1.0']['servers']
+        break
 
-    if apiroots is None:
+    if apiroots is None or len(apiroots) == 0:
         return "No API roots found!"
 
-    return "API roots: %s" % repr(apiroots)
+    args = {'limit': '10',
+            'post_types': 'https://tent.io/types/post/status/v0.1.0'}
+    headers = {'accept': tent_mime}
+    posts = None
+    for root in apiroots:
+        url = root + "/posts"
+        try:
+            r = requests.get(url, timeout=5, headers=headers, params=args)
+            r.raise_for_status()
+        except requests.exceptions.RequestException as e:
+            app.logger.debug('exception when getting %s: %s' % (url, repr(e)))
+            continue
+
+        posts = r.json
+        if posts is None:
+            app.logger.debug('%s returned no valid JSON' % url)
+        else:
+            break
+
+    return "posts: %s" % repr(posts)
 
 if __name__ == '__main__':
     app.run()
