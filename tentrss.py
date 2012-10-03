78c90be496ed5ae23a70cebdba2866c9aa1eee77
Scott Feeney <scott@oceanbase.org>
2012-10-02 20:53:23 -0500

    Find list of API roots

--- tentrss.py
+++ tentrss.py
@@ -16,12 +16,33 @@ def user_feed():
     if tent_uri == '':
         return 'No URI!'
     r = requests.get(tent_uri, timeout=5)
+
+    # Look for profile links in the HTTP "link" header and get API roots
+    # list from the first profile link that works.
+    # TODO: Should also look for HTML "link" tag in response content
+    apiroots = None
     for link in re.split(',\s*', r.headers['link']):
         href, rel = \
             re.match('''<(https?://[^>]+)>; rel="(https?://[^\"]+)"\s*$''',
                      link).groups()
         app.logger.debug('link: %s, rel=%s' % (href, rel))
-    return 'not done yet but worked so far!'
+        if rel != 'https://tent.io/rels/profile':
+            continue
+
+        headers = {'accept': 'application/vnd.tent.v0+json'}
+        try:
+            r = requests.get(href, timeout=5, headers=headers)
+        except requests.exceptions.RequestException as e:
+            app.logger.debug('exception loading %s: %s' % (href, repr(e)))
+            continue
+
+        # profile link worked, use it
+        apiroots = r.json['https://tent.io/types/info/core/v0.1.0']['servers']
+
+    if apiroots is None:
+        return "No API roots found!"
+
+    return "API roots: %s" % repr(apiroots)
 
 if __name__ == '__main__':
     app.run()
